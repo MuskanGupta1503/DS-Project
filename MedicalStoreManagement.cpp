e506c4571bd7673584c18bc385579b07f1a0fc3c
MuskanGupta1503 <66381138+MuskanGupta1503@users.noreply.github.com>
2020-09-20 00:42:56 +0530

    Added admin and customer

--- MedicalStoreManagement.cpp
+++ MedicalStoreManagement.cpp
@@ -1,8 +1,24 @@
 #include<bits/stdc++.h>
+#include<conio.h>
 using namespace std;
+void fun()
+{
+    cout<<"Hello"<<endl;
+}
 int main()
 {
-    cout<<"Hello World";
+    int n;
+    cout<<"Welcome to Medical Store"<<endl;
+    cout<<"Select"<<endl<<"1 for Customer"<<endl<<"2 for Admin"<<endl;
+    cin>>n;
+    if(n==2)
+    {
+        fun();
+    }
+    else{
+        return 0;
+    }
+
     return 0;
 }
 
