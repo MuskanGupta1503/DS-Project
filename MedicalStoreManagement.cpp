5b456a2f180e3087d6a074095bec4592f2032ad9
MuskanGupta1503 <66381138+MuskanGupta1503@users.noreply.github.com>
2020-09-20 21:50:44 +0530

    Created Basic Structure

--- MedicalStoreManagement.cpp
+++ MedicalStoreManagement.cpp
@@ -1,24 +1,125 @@
 #include<bits/stdc++.h>
-#include<conio.h>
 using namespace std;
-void fun()
+class Medicine
 {
-    cout<<"Hello"<<endl;
+private:
+    int ID,qty;
+    string name,cmpname,supname;
+    float unitCost,saleCost;
+public:
+
+   void getdata()
+   {
+     cout<<"Enter Medicine ID \n";
+     cin>>ID;
+     cout<<"Enter Medicine name \n";
+     cin>>name;
+     cout<<"Enter Company marks \n";
+     cin>>cmpname;
+     cout<<"Enter Supplier marks \n";
+     cin>>supname;
+     cout<<"Enter Qty \n";
+     cin>>qty;
+     cout<<"Enter Unit Cost \n";
+     cin>>unitCost;
+     cout<<"Enter Sale Cost \n";
+     cin>>saleCost;
+   }
+   void display()
+   {
+        cout<<endl<<"Medicine ID: "<<ID;
+        cout<<endl<<"Name: "<<name;
+        cout<<endl<<"Company name: "<<cmpname;
+        cout<<endl<<"Supplier name: "<<supname;
+        cout<<endl<<"Unit cost: "<<unitCost;
+        cout<<endl<<"Sale Cost: "<<saleCost;
+        cout<<endl<<"Quantity: "<<qty;
+        cout<<endl;
+   }
+   string retname()
+   {
+       return name;
+   }
+   int retID()
+   {
+       return ID;
+   }
+};
+char rep;
+Medicine s,s1;
+fstream fs,temp;
+void create()
+{
+    fs.open("med.txt",ios::out| ios::binary);
+    do{
+        s.getdata();
+        fs.write((char*)&s,sizeof(s));
+        cout<<endl<<"Do you want to add more records"<<endl;
+        cin>>rep;
+    }while(rep=='y' || rep=='Y');
+    fs.close();
+}
+
+void dispfile()
+{
+    fs.open("med.txt",ios::in| ios::binary);
+    while(fs.read((char*)&s,sizeof(s)))
+    {
+        s.display();
+    }
+    fs.close();
+}
+void append()
+{
+      fs.open("med.txt",ios::app|ios::binary);
+      do{
+        s.getdata();
+        fs.write((char*)&s,sizeof(s));                                                     // object's' is written into the file
+        cout<<"Do you want to enter another record (y/n)";
+        cin>>rep;
+      }while(rep=='y' || rep=='Y');
+      fs.close();
+}
+void search()
+{
+    int choice;
+    fs.open("med.txt",ios::app|ios::binary);
+    cout<<"Search on the basis of: "<<endl<<"1.roll no"<<endl<<"2.Name"<<endl;
+    cout<<"Enter Choice__";
+    cin>>choice;
+    if(choice==1)
+    {
+    }
+}
+void editrec()
+{
+
+}
+void count_rec()
+{
+
 }
 int main()
 {
-    int n;
-    cout<<"Welcome to Medical Store"<<endl;
-    cout<<"Select"<<endl<<"1 for Customer"<<endl<<"2 for Admin"<<endl;
-    cin>>n;
-    if(n==2)
-    {
-        fun();
-    }
-    else{
-        return 0;
-    }
-
+    int ch;
+    char ch1;
+    do{
+        cout<<"Menu"<<endl;
+        cout<<"1.Create a file"<<endl;
+        cout<<"2.Read a file"<<endl;
+        cout<<"3.Append a file"<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:create();
+            break;
+            case 2:dispfile();
+            break;
+            case 3:append();
+            break;
+        }
+        cout<<endl<<"Want to continue(y/n)--";
+        cin>>ch1;
+    }while(ch1=='y' || ch1=='Y');
     return 0;
 }
-
