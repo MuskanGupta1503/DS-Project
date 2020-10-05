f3168be90082b3b4a5a46819b50aacd9047555f9
MuskanGupta1503 <66381138+MuskanGupta1503@users.noreply.github.com>
2020-10-05 18:12:47 +0530

    added edit functionality

--- MedicalStoreManagement.cpp
+++ MedicalStoreManagement.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#include<iostream>
+#include<fstream>
 using namespace std;
 
 class Medicine
@@ -13,14 +15,17 @@ public:
    {
      cout<<"Enter Medicine ID \n";
      cin>>ID;
+     cin.ignore(numeric_limits<streamsize>::max(),'\n');
      cout<<"Enter Medicine name \n";
      cin>>name;
      cout<<"Enter Company name \n";
      cin>>cmpname;
      cout<<"Enter Supplier name \n";
      cin>>supname;
+     cin.ignore(numeric_limits<streamsize>::max(),'\n');
      cout<<"Enter Qty \n";
      cin>>qty;
+     cin.ignore(numeric_limits<streamsize>::max(),'\n');
      cout<<"Enter Unit Cost \n";
      cin>>unitCost;
      cout<<"Enter Sale Cost \n";
@@ -66,7 +71,7 @@ fstream fs,temp;
 
 void create()
 {
-    fs.open("med.txt",ios::out| ios::binary | ios::app);
+    fs.open("med.txt",ios::out| ios::binary | ios::app |ios::in);
     do{
         s.getdata();
         fs.write((char*)&s,sizeof(s));
@@ -78,7 +83,13 @@ void create()
 
 void dispfile()
 {
-    fs.open("med.txt",ios::in| ios::binary);
+    fs.open("med.txt",ios::in| ios::binary | ios::out| ios::app );
+    fs.seekg(0,ios::beg);
+    if(!fs)
+    {
+        cout<<"FILE does not exists"<<endl;
+        return;
+    }
     while(fs.read((char*)&s,sizeof(s)))
     {
         s.display();
@@ -88,7 +99,7 @@ void dispfile()
 
 void append()
 {
-      fs.open("med.txt",ios::app|ios::binary);
+      fs.open("med.txt",ios::app|ios::binary | ios::out | ios::in);
       do{
         s.getdata();
         fs.write((char*)&s,sizeof(s));                              // object's' is written into the file
@@ -101,22 +112,37 @@ void append()
 void search_rec()
 {
     int choice;
-    fs.open("med.txt",ios::in|ios::binary);
+    fs.open("med.txt",ios::in|ios::binary| ios::out | ios::app);
+    fs.seekg(0,ios::beg);
+    if(!fs)
+    {
+        cout<<"File Reading Error"<<endl;
+        return;
+    }
+
     cout<<"Search on the basis of: "<<endl<<"1.ID"<<endl<<"2.Name"<<endl<<"3.Company Name"<<endl<<"4.Supplier Name"<<endl;
-    cout<<"Enter Choice__"<<endl;
+    cout<<"Enter Choice__";
     cin>>choice;
+    int found=0;
     if(choice==1)
     {
         int id;
-        cout<<"Enter ID to be searched---"<<endl;
+        cout<<"Enter ID to be searched---";
         cin>>id;
+
         while(fs.read((char*)&s,sizeof(s)))
         {
             if(s.retID()==id)
             {
+                found=1;
                 s.display();
-                break;
+                fs.seekg(0,ios::beg);
+                return;
             }
+        }
+        if(found==0)
+        {
+            cout<<"No such ID found"<<endl;
         }
     }
         if(choice==2)
@@ -128,9 +154,14 @@ void search_rec()
         {
             if(s.retname()==nm)
             {
+                found=1;
                 s.display();
                 break;
             }
+        }
+         if(found==0)
+        {
+            cout<<"No such ID found"<<endl;
         }
     }
         if(choice==3)
@@ -142,9 +173,14 @@ void search_rec()
         {
             if(s.retCompname()==cmp)
             {
+                found=1;
                 s.display();
 //                break;
             }
+        }
+         if(found==0)
+        {
+            cout<<"No such ID found"<<endl;
         }
     }
         if(choice==4)
@@ -156,21 +192,66 @@ void search_rec()
         {
             if(s.retSupname()==smp)
             {
+                found=1;
                 s.display();
 //                break;
             }
         }
+         if(found==0)
+        {
+            cout<<"No such ID found"<<endl;
+        }
     }
+    fs.close();
 }
 
 void editrec()
 {
+    fs.open("med.txt",ios::app|ios::binary | ios::out | ios::in);
+     if(!fs)
+    {
+        cout<<"File Reading Error"<<endl;
+        return;
+    }
+    int id1;
+    cout<<"Enter Medicine ID to be modified "<<endl;
+    cin>>id1;
+    int rec=0;
+    int f=0;
+    while(fs.read((char*)&s,sizeof(s)))
+    {
+        if(s.retID()==id1)
+        {
+            cout<<"Enter New Information "<<endl;
+            s.getdata();
+            fs.seekg(rec*sizeof(s),ios::beg);
+            fs.write((char*)&s,sizeof(s));
+            f=1;
+            break;
+        }
+        rec++;
+    }
+    if(f==1)
+    {
+        cout<<endl<<" RECORD UPDATED "<<endl;
+    }
+    else
+    {
+        cout<<endl<<"RECORD NOT FOUND"<<endl;
+    }
+
+    fs.close();
 
 }
 
 void count_rec()
 {
     fs.open("med.txt",ios::in|ios::binary);
+     if(!fs)
+    {
+        cout<<"File Reading Error"<<endl;
+        return;
+    }
     int num=0;
     while(fs.read((char*)&s,sizeof(s)))
     {
@@ -191,20 +272,28 @@ int main()
         cout<<"3.Append a file"<<endl;
         cout<<"4.Count total records in a file"<<endl;
         cout<<"5.Search and display a record"<<endl;
-        cout<<"6.Edit a record"<<endl;
+        cout<<"6.Update a record on the basis of entered ID"<<endl;
         cin>>ch;
         switch(ch)
         {
-            case 1:create();
-            break;
-            case 2:dispfile();
-            break;
-            case 3:append();
-            break;
-            case 4:count_rec();
-            break;
-            case 5:search_rec();
-            break;
+            case 1:
+                create();
+                break;
+            case 2:
+                dispfile();
+                break;
+            case 3:
+                append();
+                break;
+            case 4:
+                count_rec();
+                break;
+            case 5:
+                search_rec();
+                break;
+            case 6:
+                editrec();
+                break;
         }
         cout<<endl<<"Want to continue(y/n)--";
         cin>>ch1;
